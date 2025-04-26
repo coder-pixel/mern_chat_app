19400621c5e68e7918b7c1739d04de1e2a346aa6
sauvik <sauvik@logic-square.com>
2025-04-26 17:13:13 +0530

    fix: show logged in user name at app startup

--- frontend/src/components/Messages/MessageContainer.jsx
+++ frontend/src/components/Messages/MessageContainer.jsx
@@ -12,6 +12,7 @@ import useConversation from "../../zustand/useConversation";
 // import useWebRTC from "../../hooks/useWebRTC"; // Remove hook import
 import { useSocketContext } from "../../context/SocketContext";
 import CallButtons from "../CallButtons";
+import { useAuthContext } from "../../context/AuthContext";
 
 const MessageContainer = ({
   localStream,
@@ -208,10 +209,12 @@ const MessageContainer = ({
 export default MessageContainer;
 
 const NoChatSelected = () => {
+  const { authUser } = useAuthContext();
+
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-        <p>Welcome 👋 John Doe ❄</p>
+        <p>Welcome 👋 {authUser?.fullName || ""} ❄</p>
         <p>Select a chat to start messaging</p>
         <TiMessages className="text-3xl md:text-6xl text-center" />
       </div>
